ab68a268492e1f036c3c6cb8504d4918aeabd3b6
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-04-27 13:21:58 -0300

    Adição do módulo de correção para FakePlayers

--- src/main/java/team/martin/hfix/hFix.java
+++ src/main/java/team/martin/hfix/hFix.java
@@ -1,19 +1,39 @@
 package team.martin.hfix;
 
+/**
+ *
+ * O plugin foi feito com o intuíto de arrumar alguns bugs e adicionar funcionalidades para os servidoes,
+ * Caro desenvolvedor, permito apenas a criação de classes no projeto para você implementar novos recursos.
+ * E sempre documente alterações.
+ *
+ * @version 1.1
+ * @author Martinfer22
+ */
+
+
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
-import team.martin.hfix.util.ConfigUtils;
+import team.martin.hfix.util.LogUtils;
 
 public class hFix extends JavaPlugin {
 
+    LogUtils log = new LogUtils();
+
     @Override
     public void onEnable() {
-        ConfigUtils cfg = new ConfigUtils();
-        cfg.getEventos();
-        saveDefaultConfig();
+        try {
+            log.getInit();
+            saveDefaultConfig();
+        } catch (Exception e){
+            System.out.println(ChatColor.RED + "Algo deu errado na inicialização.");
+            System.out.println(ChatColor.RED + "Reporte as frases abaixo na github https://github.com/TheMartinfer22/hFix");
+            e.getStackTrace();
+        }
     }
 
     @Override
     public void onDisable() {
+        log.getExit();
     }
 
     public static hFix getPluginMainClass(){
