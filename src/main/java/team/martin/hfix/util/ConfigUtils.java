8a02b6bebd17a8d5c53162295de288ecc5f67a07
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-05-13 13:06:26 -0300

    Criação da funcionalidade de banir blocos, compatível com mods e documentação para iniciantes.

--- src/main/java/team/martin/hfix/util/ConfigUtils.java
+++ src/main/java/team/martin/hfix/util/ConfigUtils.java
@@ -3,38 +3,56 @@ package team.martin.hfix.util;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
-import team.martin.hfix.bloqueios.AntiDanoDoVoid;
-import team.martin.hfix.bloqueios.AntiTetoDoNether;
+import team.martin.hfix.bloqueios.NoVoidDamage;
+import team.martin.hfix.bloqueios.NoNetherRoof;
+import team.martin.hfix.fixes.BanThisBlock;
 import team.martin.hfix.fixes.FakePlayers;
 import team.martin.hfix.hFix;
 
 import java.util.Arrays;
 
 public class ConfigUtils{
-    PluginManager pm = Bukkit.getServer().getPluginManager();
+    /*
+     * Não se assuste com essa classe, apenas é para administrar o arquivo config.yml.
+     */
+    PluginManager pm = Bukkit.getServer().getPluginManager(); // Irá pegar a classe Main do projeto.
 
 
     public String getFraseAntiTetoDoNether(){
-        return hFix.getPluginMainClass().getConfig().getString("raseAntiTetoDoNetherTOP");
+        return hFix.getPluginMainClass().getConfig().getString("FraseAntiTetoDoNetherTOP"); // Irá pegar na config.yml a opção FraseAntiTetoDoNetherTOP
     }
+
     public String getFraseAntiNetherYDOWN(){
-        return hFix.getPluginMainClass().getConfig().getString("raseAntiTetoDoNetherDOWN");
+        return hFix.getPluginMainClass().getConfig().getString("FraseAntiTetoDoNetherDOWN");
     }
+
     public String getPermissaoAntiVoid(){
         return hFix.getPluginMainClass().getConfig().getString("PermissaoAntiVoid");
     }
+
+    public String getFraseDeBanidos(){
+        return hFix.getPluginMainClass().getConfig().getString("FraseDeBanidos");
+    }
+
     public String getFakePayers(){
-        String[] playersOFF = {hFix.getPluginMainClass().getConfig().getString("FakePlayers")};
+        String[] playersOFF = {hFix.getPluginMainClass().getConfig().getString("FakePlayers")}; // Irá pegar na config.yml uma lista de Strings.
         return Arrays.toString(playersOFF);
     }
 
+    public String getBlocosBanidos(){
+        String[] blocos = {hFix.getPluginMainClass().getConfig().getString("BlocosBanidos")};
+        return Arrays.toString(blocos);
+    }
 
+    /*
+     * getEventos, fará a execução da listagem de módulos do plugin.
+     */
     public void getEventos(){
-        if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarAntiTetoNether")){
+        if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarAntiTetoNether")){ // Pegará na config.yml se a booleana AtivarAntiTetoNether está true
             try {
-                pm.registerEvents((Listener) new AntiTetoDoNether(), (hFix.getPluginMainClass()));
+                pm.registerEvents((Listener) new NoNetherRoof(), (hFix.getPluginMainClass())); // Casp esteja irá instanciar a classe NoNetherRoof
                 System.out.println("[+] Módulo AntiTetoNether.");
-            } catch (Exception e) {
+            } catch (Exception e) { // Para qualquer erro, irá apenas dizer que não é compatível.
                 System.out.println("*** Módulo de AntiTetoNether não compatível.");
             }
         }
@@ -48,10 +66,18 @@ public class ConfigUtils{
         }
         if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarAntiDanoDoVoid")){
             try {
-                pm.registerEvents((Listener) new AntiDanoDoVoid(), (hFix.getPluginMainClass()));
+                pm.registerEvents((Listener) new NoVoidDamage(), (hFix.getPluginMainClass()));
                 System.out.println("[+] Módulo AntiDanoDoVoid.");
             } catch (Exception e){
                 System.out.println("*** Módulo de AntiDanoDoVoid não compatível.");
+            }
+        }
+        if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarBanimentosDeBlocos")){
+            try {
+                pm.registerEvents((Listener) new BanThisBlock(), (hFix.getPluginMainClass()));
+                System.out.println("[+] Módulo de Banimentos de blocos.");
+            } catch (Exception e){
+                System.out.println("*** Módulo de Banimentos de blocos não compatível.");
             }
         }
 
