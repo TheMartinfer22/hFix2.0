3097e734cf74c676fffbe310fab7cb1842362e27
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-05-13 13:12:59 -0300

    Documentação para iniciantes.

--- src/main/java/team/martin/hfix/hFix.java
+++ src/main/java/team/martin/hfix/hFix.java
@@ -6,7 +6,7 @@ package team.martin.hfix;
  * Caro desenvolvedor, permito apenas a criação de classes no projeto para você implementar novos recursos.
  * E sempre documente alterações.
  *
- * @version 1.1
+ * @version 1.4
  * @author Martinfer22
  */
 
@@ -23,25 +23,23 @@ public class hFix extends JavaPlugin {
     @Override
     public void onEnable() {
         try {
-            log.getInit();
-            saveDefaultConfig();
-            int pluginId = 11241;
-            new Metrics(this, pluginId);
+            log.getInit(); // Inicializador.
+            saveDefaultConfig(); // Salvando configuração padrão.
+
+            int pluginId = 11241; // Metrics bStats
+            new Metrics(this, pluginId); // Metrics bStats
+
         } catch (Exception e){
             System.out.println(ChatColor.RED + "Algo deu errado na inicialização.");
             System.out.println(ChatColor.RED + "Reporte as frases abaixo na github https://github.com/TheMartinfer22/hFix");
             e.getStackTrace();
         }
     }
-
     @Override
     public void onDisable() {
         log.getExit();
     }
-
     public static hFix getPluginMainClass(){
         return getPlugin(hFix.class);
     }
-
-
 }
