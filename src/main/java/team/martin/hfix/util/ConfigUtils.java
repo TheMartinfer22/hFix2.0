a99dd599e2968e384a2ee05c78b13d5481aeb7c4
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-09 18:20:52 -0300

    Mudança de linguagem e adição de frase para void damage.

--- src/main/java/team/martin/hfix/util/ConfigUtils.java
+++ src/main/java/team/martin/hfix/util/ConfigUtils.java
@@ -3,10 +3,10 @@ package team.martin.hfix.util;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
-import team.martin.hfix.bloqueios.NoVoidDamage;
-import team.martin.hfix.bloqueios.NoNetherRoof;
-import team.martin.hfix.fixes.BanThisBlock;
-import team.martin.hfix.fixes.FakePlayers;
+import team.martin.hfix.events.NoVoidDamage;
+import team.martin.hfix.events.NoNetherRoof;
+import team.martin.hfix.events.BanThisBlock;
+import team.martin.hfix.events.FakePlayers;
 import team.martin.hfix.hFix;
 
 import java.util.Arrays;
@@ -19,19 +19,23 @@ public class ConfigUtils{
 
 
     public String getFraseAntiTetoDoNether(){
-        return hFix.getPluginMainClass().getConfig().getString("FraseAntiTetoDoNetherTOP"); // Irá pegar na config.yml a opção FraseAntiTetoDoNetherTOP
+        return hFix.getPluginMainClass().getConfig().getString("PhraseAntiCeilingDoNetherTOP"); // Irá pegar na config.yml a opção FraseAntiTetoDoNetherTOP
     }
 
     public String getFraseAntiNetherYDOWN(){
-        return hFix.getPluginMainClass().getConfig().getString("FraseAntiTetoDoNetherDOWN");
+        return hFix.getPluginMainClass().getConfig().getString("PhraseAntiCeilingDoNetherDOWN");
     }
 
     public String getPermissaoAntiVoid(){
-        return hFix.getPluginMainClass().getConfig().getString("PermissaoAntiVoid");
+        return hFix.getPluginMainClass().getConfig().getString("PermissionAntiVoid");
+    }
+
+    public String getAntiVoidPhrase(){
+        return hFix.getPluginMainClass().getConfig().getString("AntiVoidPhrase");
     }
 
     public String getFraseDeBanidos(){
-        return hFix.getPluginMainClass().getConfig().getString("FraseDeBanidos");
+        return hFix.getPluginMainClass().getConfig().getString("BannedPhrase");
     }
 
     public String getFakePayers(){
@@ -40,7 +44,7 @@ public class ConfigUtils{
     }
 
     public String getBlocosBanidos(){
-        String[] blocos = {hFix.getPluginMainClass().getConfig().getString("BlocosBanidos")};
+        String[] blocos = {hFix.getPluginMainClass().getConfig().getString("BannedBlocks")};
         return Arrays.toString(blocos);
     }
 
@@ -48,33 +52,33 @@ public class ConfigUtils{
      * getEventos, fará a execução da listagem de módulos do plugin.
      */
     public void getEventos(){
-        if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarAntiTetoNether")){ // Pegará na config.yml se a booleana AtivarAntiTetoNether está true
+        if (hFix.getPluginMainClass().getConfig().getBoolean("EnableAntiCeilingNether")){ // Pegará na config.yml se a booleana AtivarAntiTetoNether está true
             try {
-                pm.registerEvents((Listener) new NoNetherRoof(), (hFix.getPluginMainClass())); // Casp esteja irá instanciar a classe NoNetherRoof
+                pm.registerEvents(new NoNetherRoof(), (hFix.getPluginMainClass())); // Casp esteja irá instanciar a classe NoNetherRoof
                 System.out.println("[+] Módulo AntiTetoNether.");
             } catch (Exception e) { // Para qualquer erro, irá apenas dizer que não é compatível.
                 System.out.println("*** Módulo de AntiTetoNether não compatível.");
             }
         }
-        if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarFakePlayers")){
+        if (hFix.getPluginMainClass().getConfig().getBoolean("EnableFakePlayers")){
             try {
-                pm.registerEvents((Listener) new FakePlayers(), (hFix.getPluginMainClass()));
+                pm.registerEvents(new FakePlayers(), (hFix.getPluginMainClass()));
                 System.out.println("[+] Módulo FakePlayers.");
             } catch (Exception e) {
                 System.out.println("*** Módulo de FakePlayers não compatível.");
             }
         }
-        if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarAntiDanoDoVoid")){
+        if (hFix.getPluginMainClass().getConfig().getBoolean("EnableAntiDanoDoVoid")){
             try {
-                pm.registerEvents((Listener) new NoVoidDamage(), (hFix.getPluginMainClass()));
+                pm.registerEvents(new NoVoidDamage(), (hFix.getPluginMainClass()));
                 System.out.println("[+] Módulo AntiDanoDoVoid.");
             } catch (Exception e){
                 System.out.println("*** Módulo de AntiDanoDoVoid não compatível.");
             }
         }
-        if (hFix.getPluginMainClass().getConfig().getBoolean("AtivarBanimentosDeBlocos")){
+        if (hFix.getPluginMainClass().getConfig().getBoolean("EnableBlockBans")){
             try {
-                pm.registerEvents((Listener) new BanThisBlock(), (hFix.getPluginMainClass()));
+                pm.registerEvents(new BanThisBlock(), (hFix.getPluginMainClass()));
                 System.out.println("[+] Módulo de Banimentos de blocos.");
             } catch (Exception e){
                 System.out.println("*** Módulo de Banimentos de blocos não compatível.");
