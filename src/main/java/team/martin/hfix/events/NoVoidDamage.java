28c22dfef6da60dcb638765e1231c3ce44d23d2d
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-09 18:21:51 -0300

    Bug de danos por outros mods foi arrumado e adição de nova sub-feature.

---
+++ src/main/java/team/martin/hfix/events/NoVoidDamage.java
@@ -0,0 +1,31 @@
+package team.martin.hfix.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import team.martin.hfix.util.ConfigUtils;
+
+/*
+ * Esses eventos estão como alta prioridade de execução para que possa ler cada ação,
+ * Não causará problemas de desempenho.
+ */
+
+public class NoVoidDamage extends ConfigUtils implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void antiDanoDoVoid(EntityDamageEvent event){
+        if (event.getEntity().hasPermission(getPermissaoAntiVoid()) && (event.getEntity().getLocation().getY() < -35)){
+            Location spawnpoint = Bukkit.getServer().getWorld("world").getSpawnLocation();
+            event.setCancelled(true);
+            try {
+                event.getEntity().teleport(spawnpoint);
+                event.getEntity().sendMessage(getAntiVoidPhrase());
+            } catch(NullPointerException ex){
+                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "spawn " + event.getEntity().getName());
+            }
+        }
+    }
+}
