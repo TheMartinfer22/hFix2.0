efc6e9041ebbad9ecf4370e338d419cbaa35cace
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-14 11:06:48 -0300

    Criação do recurso de limitar mobs por chunk. (WIP)

---
+++ src/main/java/team/martin/hfix/config/LimitMobChunkConfig.java
@@ -0,0 +1,24 @@
+package team.martin.hfix.config;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import team.martin.hfix.events.LimitMobChunk;
+import team.martin.hfix.hFix;
+
+public class LimitMobChunkConfig {
+    PluginManager pm = Bukkit.getServer().getPluginManager();
+    public void enable(){
+        if (hFix.getPluginMainClass().getConfig().getBoolean("EnableLimitMobChunk")){
+            try {
+                pm.registerEvents(new LimitMobChunk(), (hFix.getPluginMainClass()));
+                System.out.println("[+] Módulo LimitMobChunk.");
+            } catch (Exception e) {
+                System.out.println("*** Módulo de LimitMobChunk não compatível.");
+            }
+        }
+    }
+
+    public int getMobsPerChunk(){
+        return hFix.getPluginMainClass().getConfig().getInt("MobsPerChunk");
+    }
+}
---
+++ src/main/java/team/martin/hfix/events/LimitMobChunk.java
@@ -0,0 +1,18 @@
+package team.martin.hfix.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import team.martin.hfix.config.LimitMobChunkConfig;
+
+public class LimitMobChunk extends LimitMobChunkConfig implements Listener {
+    @EventHandler
+    public void checkMobs(EntitySpawnEvent event){
+        Location location = event.getLocation();
+        if (location.getChunk().getEntities().length >= getMobsPerChunk()){
+            event.setCancelled(true);
+        }
+    }
+}
