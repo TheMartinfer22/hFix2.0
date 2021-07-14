4315f862cd8ff31c87f0edac0892a61621e1032a
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-14 02:11:06 -0300

    👩‍🔧 Added ClearMobsFunction to accept argument

--- src/main/java/team/martin/hfix/functions/ClearMobsFunction.java
+++ src/main/java/team/martin/hfix/functions/ClearMobsFunction.java
@@ -7,18 +7,26 @@ import org.bukkit.entity.Player;
 import team.martin.hfix.config.BlackListConfig;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class ClearMobsFunction extends BlackListConfig{
+    String mobs = Arrays.asList(getMobs()).toString().replace("]]", "").replace("[[", "");
     public void run(){
-        String mobs = Arrays.asList(getMobs()).toString().replace("]]", "").replace("[[", "");
-
         Bukkit.getServer().getWorlds().forEach(world -> {
             world.getEntities().forEach(entity -> {
-                if (!(mobs.equals(entity.getType().toString()) || entity instanceof Player || entity.getCustomName() != null || entity instanceof Item)){
+                if (!((mobs.contains(entity.getType().toString())) || entity instanceof Player || entity.getCustomName() != null || entity instanceof Item)){
                     entity.remove();
                 }
             });
             System.out.println(ChatColor.GREEN + "[hFix] Was cleaned " + world.getEntities().size() + " entities [" + world.getName() + "]");
+        });
+    }
+
+    public void run(String world){
+        Objects.requireNonNull(Bukkit.getWorld(world)).getEntities().forEach(entity -> {
+            if (!((mobs.contains(entity.getType().toString())) || entity instanceof Player || entity.getCustomName() != null || entity instanceof Item)){
+                entity.remove();
+            }
         });
     }
 }
