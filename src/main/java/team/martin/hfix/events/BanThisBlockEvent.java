e6de8a1486c95f445d6231ccb63323cbfcaf37ff
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-14 02:09:53 -0300

    👩‍🔧 Fix 1.12.2 issue

--- src/main/java/team/martin/hfix/events/BanThisBlockEvent.java
+++ src/main/java/team/martin/hfix/events/BanThisBlockEvent.java
@@ -12,12 +12,11 @@ import team.martin.hfix.config.BanThisBlockConfig;
  * Esses eventos estão como alta prioridade de execução para que possa ler cada ação,
  * Não causará problemas de desempenho.
  */
-
 public class BanThisBlockEvent extends BanThisBlockConfig implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onBlockPlace(BlockPlaceEvent evento) {
-        if (evento.getBlock().getBlockData().getAsString().equals(getBlocosBanidos()
+        if (evento.getBlock().getType().toString().equals(getBlocosBanidos()
                 .replace("[[", "")
                 .replace("]]", "")) && (!evento.getPlayer().isOp())) {
             evento.setCancelled(true);
@@ -27,7 +26,7 @@ public class BanThisBlockEvent extends BanThisBlockConfig implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onBlockBreak(BlockBreakEvent evento) {
-        if (evento.getBlock().getBlockData().getAsString().equals(getBlocosBanidos()
+        if (evento.getBlock().getType().toString().equals(getBlocosBanidos()
                 .replace("[[", "")
                 .replace("]]", "")) && (!evento.getPlayer().isOp())) {
             evento.setCancelled(true);
