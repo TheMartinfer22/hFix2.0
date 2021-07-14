d518e9b372aaa9fffee49eee1ea4c954c0408951
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-07-14 02:11:24 -0300

    👩‍🔧 Fix issues on instances

--- src/main/java/team/martin/hfix/util/ConfigUtils.java
+++ src/main/java/team/martin/hfix/util/ConfigUtils.java
@@ -1,40 +1,44 @@
 package team.martin.hfix.util;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
-import team.martin.hfix.commands.MenuCommand;
+import org.jsoup.Jsoup;
+import team.martin.hfix.commands.MenuCommandChat;
 import team.martin.hfix.config.*;
-import team.martin.hfix.events.BanThisBlockEvent;
-import team.martin.hfix.events.extras.MenuListenerEvent;
-import team.martin.hfix.events.extras.VersionCheckerEvent;
 import team.martin.hfix.hFix;
+
+import java.io.IOException;
 
 public class ConfigUtils{
     PluginManager pm = Bukkit.getServer().getPluginManager();
 
+
     /*
      * getEventos, fará a execução da listagem de módulos do plugin.
      */
-    public void getEventos(){
+    public void getEventos() throws IOException {
         System.out.println("Versão do servidor: " + hFix.getPluginMainClass().getServer().getVersion());
-        pm.registerEvents(new VersionCheckerEvent(), (hFix.getPluginMainClass()));
 
-        if (!Bukkit.getServer().getVersion().contains("(MC: 1.12.2)")){
-            try {
-                pm.registerEvents(new MenuListenerEvent(), (hFix.getPluginMainClass()));
-                hFix.getPluginMainClass().getCommand("hfix").setExecutor(new MenuCommand());
-                new BanThisBlockConfig().enable();
-            } catch (Exception e){
-                System.out.println("*** Desablitando alguns módulos (Menu, BanThisBlock)");
-            }
+        // Version Manager
+        String getVersion  = Jsoup.connect("https://themartinfer22.github.io/hFix/").get().title();
+        String version = "1.9.1";
+        if (!getVersion.contains(version)){
+            System.out.println(ChatColor.RED + "----------------------------------------------------------");
+            System.out.println(ChatColor.RED + "[hFix] You are using an old version of the hFix plugin, update and don't forget to delete its folder.");
+            System.out.println(ChatColor.RED + "----------------------------------------------------------");
         }
 
         // Carregamento das config's
-
         new FakePlayersConfig().enable();
         new LagVerifyConfig().enable();
         new LimitMobChunkConfig().enable();
         new NoNetherRoofConfig().enable();
         new NoVoidDamageConfig().enable();
+        new BanThisBlockConfig().enable();
+
+
+        // Carregamento dos comandos
+        hFix.getPluginMainClass().getCommand("hfix").setExecutor(new MenuCommandChat());
     }
 }
