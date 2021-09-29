3ce91dcbc20aa464a35f230e5a373a977036830e
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-09-29 18:10:48 -0300

    ✨ Limit feature renew 2.0

--- src/main/java/dev/nanosync/hfix/features/mob/LimitMobsFeature.java
+++ src/main/java/dev/nanosync/hfix/features/mob/LimitMobsFeature.java
@@ -1,19 +1,24 @@
-package team.martin.hfix.features.mob;
+package dev.nanosync.hfix.features.mob;
 
+import dev.nanosync.hfix.api.ConfigAPI;
+import dev.nanosync.hfix.types.ConfigType;
+import dev.nanosync.hfix.fabric.ChunkFabric;
+import dev.nanosync.hfix.fabric.WorldFabric;
 import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
-import team.martin.hfix.fabric.ChunkFabric;
-import team.martin.hfix.fabric.WorldFabric;
 
 public class LimitMobsFeature implements Listener {
+
+    private final Integer chunkMobLimit = new ConfigAPI(ConfigType.CHUNK).getValueAsInteger("MobsLimit");
+    private final Integer worldMobLimit = new ConfigAPI(ConfigType.WORLD).getValueAsInteger("MobsLimit");
 
     @EventHandler
     public void chunkLimiter(EntitySpawnEvent event){
         Chunk chunk = event.getLocation().getChunk();
-        if (new ChunkFabric(chunk).getMobs().size() > 10){
+        if (new ChunkFabric(chunk).getMobs().size() > chunkMobLimit){
             event.setCancelled(true);
         }
     }
@@ -21,7 +26,7 @@ public class LimitMobsFeature implements Listener {
     @EventHandler
     public void worldLimiter(EntitySpawnEvent event){
         World world = event.getLocation().getWorld();
-        if (new WorldFabric(world).getMobs().size() > 10){
+        if (new WorldFabric(world).getMobs().size() > worldMobLimit){
             event.setCancelled(true);
         }
     }
